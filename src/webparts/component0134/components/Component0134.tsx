c0eb37fab4346bdc8cb873b504251bcfcb45b8d3
Anubhav Shukla <anubhav@smalsus.com>
2022-11-11 15:43:20 +0530

    first commit

---
+++ src/webparts/component0134/components/Component0134.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { IComponent0134Props } from './IComponent0134Props';
+import Popofunctionlity from './Popofunctionlity';
+import  PopupGfg  from './TextEditor';
+
+export default class Component0134 extends React.Component<IComponent0134Props, {}> {
+  public render(): React.ReactElement<IComponent0134Props> {
+    
+
+    return (
+      <div>
+      <div>
+        <h5>Portfolios</h5>
+        <h1>Component Portfolio <PopupGfg/></h1>
+      </div>
+      
+      <Popofunctionlity/>
+    </div>
+    );
+  }
+}
---
+++ src/webparts/component0134/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IComponent0134WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'Component0134WebPartStrings' {
+  const strings: IComponent0134WebPartStrings;
+  export = strings;
+}
